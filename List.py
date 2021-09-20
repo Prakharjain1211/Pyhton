647874098d7bd509f41ab5a95d5e1a4f647376c3
Prakharjain1211 <88723250+Prakharjain1211@users.noreply.github.com>
2021-09-20 18:12:53 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,105 @@
+#  Write a program to demonstrate list in python and list comparision
+
+# empty list
+my_list = []
+
+# list of integers
+my_list = [1, 2, 3]
+
+# list with mixed data types
+my_list = [1, "Hello", 3.4]
+
+# nested list
+my_list = ["mouse", [8, 4, 6], ['a']]
+
+# List indexing
+
+my_list = ['p', 'r', 'o', 'b', 'e']
+
+print(my_list[0])  # p
+
+print(my_list[2])  # o
+
+print(my_list[4]) # e
+
+# Nested List
+n_list = ["Happy", [2, 0, 1, 5]]
+
+# Nested indexing
+print(n_list[0][1])
+
+print(n_list[1][3])
+
+# Negative indexing in lists
+my_list = ['p','r','o','b','e']
+
+print(my_list[-1])
+
+print(my_list[-5])
+
+# List slicing in Python
+
+my_list = ['p','r','o','g','r','a','m','i','z']
+
+# includes element at index 2, 3, 4
+# excludes element at index 5
+print(my_list[2:5])
+
+# elements beginning to 4th
+print(my_list[:-5])
+
+# elements 6th to end
+print(my_list[5:])
+
+# elements beginning to end
+print(my_list[:])
+
+# Correcting mistake values in a list
+odd = [2, 4, 6, 8]
+
+# change the 1st item    
+odd[0] = 1            
+
+print(odd)
+
+# change 2nd to 4th items
+odd[1:4] = [3, 5, 7]  
+
+print(odd)  
+
+# Appending and Extending lists in Python
+odd = [1, 3, 5]
+
+odd.append(7)
+
+print(odd)
+
+odd.extend([9, 11, 13])
+
+print(odd)
+
+# Demonstration of list insert() method
+odd = [1, 9]
+odd.insert(1,3)
+
+print(odd)
+
+odd[2:2] = [5, 7]
+
+print(odd)
+
+# Deleting list items
+my_list = ['p', 'r', 'o', 'b', 'l', 'e', 'm']
+
+# delete one item
+del my_list[2]
+
+print(my_list)
+
+# delete multiple items
+del my_list[1:5]
+
+print(my_list)
+
+# delete entire list
+del my_list
